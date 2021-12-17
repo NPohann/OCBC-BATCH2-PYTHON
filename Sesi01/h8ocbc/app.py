20aac5d2816d9290581ddc42e6becc9acd5204f0
NPohann <npohan44@gmail.com>
2021-12-17 17:39:27 +0700

    Update Sesi 1, Sesi 2, & Assignment 1

---
+++ Sesi01/h8ocbc/app.py
@@ -0,0 +1,59 @@
+# Variabel pada python
+
+name = "Hactiv8"
+age = 54
+has_laptops = True
+print(name, age, has_laptops)
+
+
+# Penamaan variabel pada python
+age = 1
+Age = 2
+aGe = 3
+AGE = 4
+
+print(age, Age, aGe, AGE)
+
+# Operator pada python
+a = 10
+b = 5
+print(a*b)
+print(a**b)
+print(a/b)
+print(a//b)
+
+print(a==b)
+print(a==b+5)
+
+# Python List
+a = [
+    "foo",
+    "bar",
+    "baz",
+    "qux"
+]
+
+a[2] = 10
+a[-1] = 6
+a[-3] = "Hola"
+a.append("Bro")
+print(a)
+
+
+# Python Tuple
+t = ("foo", "bar", "baz", "qux", "quux", "corge")
+(s1,s2,s3,s4) = ("foo", "bar", "baz", "qux")
+print(t)
+
+# Python Dictionary
+MLB_team = {
+    "Colorado": "Rookies",
+    "Boston": "Red Sox",
+    "Minnesota": "Twins"
+}
+
+MLB_team['Kansas City'] = "Royals"
+
+print(MLB_team)
+
+# 
